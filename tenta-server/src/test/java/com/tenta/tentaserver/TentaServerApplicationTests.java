94d034733d287fd9ad938e77035a02c39e48d87b
JJL <rhqnrdl625@naver.com>
2021-01-11 21:54:18 +0900

    feat: 서버 프로젝트 환경설정

---
+++ tenta-server/src/test/java/com/tenta/tentaserver/TentaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenta.tentaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TentaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
