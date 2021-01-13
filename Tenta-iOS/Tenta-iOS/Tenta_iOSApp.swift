0cf33447662867ca1f4a23e99cbbf36505e60e60
SeokHoKim <seokho0330@gmail.com>
2021-01-13 12:45:32 +0900

    feat: iOS 프로젝트 초기화

---
+++ Tenta-iOS/Tenta-iOS/Tenta_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tenta_iOSApp.swift
+//  Tenta-iOS
+//
+//  Created by 김석호 on 2021/01/13.
+//
+
+import SwiftUI
+
+@main
+struct Tenta_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
