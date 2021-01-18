21f615018364c86dbc0871feb7fb8691a54938dd
SeokHoKim <seokho0330@gmail.com>
2021-01-18 23:45:36 +0900

    feat: LoginView 구현

---
+++ Tenta-iOS/Tenta-iOS/Views/Login/LoginView.swift
@@ -0,0 +1,32 @@
+//
+//  LoginView.swift
+//  Tenta-iOS
+//
+//  Created by 김석호 on 2021/01/18.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Tenta")
+                .font(.largeTitle)
+                .bold()
+            
+            Spacer()
+            
+            GithubLoginButton()
+                .aspectRatio(10, contentMode: .fit)
+                .padding([.leading, .trailing], 30)
+                .padding([.top, .bottom], 20)
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
