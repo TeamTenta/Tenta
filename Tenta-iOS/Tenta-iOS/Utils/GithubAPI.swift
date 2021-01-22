9c548da5f9c7f8626cd6ba152fb692f255fd6ddb
SeokHoKim <seokho0330@gmail.com>
2021-01-23 01:26:19 +0900

    feat: GithubAPIManager 추가

---
+++ Tenta-iOS/Tenta-iOS/Utils/GithubAPI.swift
@@ -0,0 +1,38 @@
+//
+//  GithubAPI.swift
+//  Tenta-iOS
+//
+//  Created by 김석호 on 2021/01/18.
+//
+
+import Foundation
+import AuthenticationServices
+
+protocol GithubLogin {
+    var githubLoginURL: URL? { get }
+
+    func githubLoginCompletion(_ url: URL)
+}
+
+final class GithubAPIManager {
+    @Published var token: String?
+}
+
+// MARK: Github Login
+extension GithubAPIManager: GithubLogin {
+    var githubLoginURL: URL? {
+        let clientID = ""
+        let scope = "repo, user"
+        let urlString = "https://github.com/login/oauth/authorize"
+        var components = URLComponents(string: urlString)!
+        components.queryItems = [
+            URLQueryItem(name: "client_id", value: clientID),
+            URLQueryItem(name: "scope", value: scope)
+        ]
+        return components.url
+    }
+
+    func githubLoginCompletion(_ url: URL) {
+        print(url)
+    }
+}
