1cac300f98190e23680c5cfb2e48ec62f74b0ce4
SeokHoKim <seokho0330@gmail.com>
2021-01-23 01:28:41 +0900

    test: LoginViewModelTest 추가

---
+++ Tenta-iOS/Tenta-iOSTests/LoginViewModelTest.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewModelTest.swift
+//  Tenta-iOSTests
+//
+//  Created by 김석호 on 2021/01/23.
+//
+
+import XCTest
+@testable import Tenta_iOS
+
+class LoginViewModelTest: XCTestCase {
+    func test_githubLoginCompletion_동작() {
+        let exp = XCTestExpectation(description: "test completion")
+        defer { wait(for: [exp], timeout: 5) }
+
+        let mockCompletion: (URL) -> Void = { _ in
+            exp.fulfill()
+        }
+
+        let viewModel = LoginViewModel(MockGithubLogin(completion: mockCompletion))
+        viewModel.completion(URL(string: "https://www.apple.com/")!)
+    }
+}
