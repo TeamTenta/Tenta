099ed8eca774a8d4c9e589d3dec70d33ca86bff2
JJL <rhqnrdl625@naver.com>
2021-01-22 20:32:49 +0900

    feat: @EnableWebSecurity 추가

--- tenta-server/src/main/java/com/tenta/tentaserver/config/WebSecurityConfig.java
+++ tenta-server/src/main/java/com/tenta/tentaserver/config/WebSecurityConfig.java
@@ -5,10 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
