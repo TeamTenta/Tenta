af95698de6628b2ae766b8418cb41d78099982cd
JJL <rhqnrdl625@naver.com>
2021-01-25 20:47:57 +0900

    feat: MessengerController 추가

---
+++ tenta-server/src/main/java/com/tenta/tentaserver/controller/MessengerController.java
@@ -0,0 +1,33 @@
+package com.tenta.tentaserver.controller;
+
+import com.tenta.tentaserver.domain.ChatDTO;
+import com.tenta.tentaserver.domain.RoomDTO;
+import com.tenta.tentaserver.domain.UserDTO;
+import com.tenta.tentaserver.service.MessengerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MessengerController {
+
+    @GetMapping("/users/{username}/messenger/rooms")
+    public List<RoomDTO> getRooms(@PathVariable String username) {
+        return null;
+    }
+
+    @PostMapping("/messenger/rooms")
+    public void createRoom(@RequestBody UserDTO receiver) {
+
+    }
+
+    @PatchMapping("/messenger/rooms/{room_id}")
+    public void updateRoom(@PathVariable("room_id") long roomId) {
+
+    }
+
+    @GetMapping("/messenger/rooms/{room_id}/chats")
+    public List<ChatDTO> getChats(@PathVariable("room_id") long roomId) {
+        return null;
+    }
+}
